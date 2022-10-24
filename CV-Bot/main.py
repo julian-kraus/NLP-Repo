81d6b957aa0cfdb628ab2efe0d97669e11e04504
DESKTOP-O10O8FO\kraus <j.kraus@tum.de>
2022-10-24 18:21:38 +0200

    add mail

--- CV-Bot/main.py
+++ CV-Bot/main.py
@@ -5,32 +5,40 @@ import re
 def check_for_alphabet(string):
     return True
 
+
 name_re = "([A-Z]|[a-z])[a-z]+ ([A-Z]|[a-z])[a-z]+( ([A-Z]|[a-z])[a-z]+)*"
 date_re = "([0-3]?[0-9].[0-1]?[0-9].[1-2][0-9][0-9][0-9])|([0-3]?[0-9]/[0-1]?[0-9]/[1-2][0-9][0-9][0-9])"
+mail_re = ".+@.+\..+"
 data = {"Personal Data":
-            {"Name": ["What is your full name?", name_re, None, None],
-             "Birthdate": ["What is your date of birth?", date_re, None]}
+            {
+            "Name": ["What is your full name?", name_re, None, None],
+            "Birthdate": ["What is your date of birth?", date_re, None],
+            "E-Mail": ["Please tell me your email.", mail_re, None]
+             }
         }
 question_num = 0
 re_num = 1
 data_num = 2
+
+
 def check_answer(answer, regex):
     if regex:
         val = re.search(regex, answer)
         print(val)
     return answer
+
+
 def ask_question(stage):
     current_stage = data[stage]
     for attr in current_stage.keys():
         answer = input(current_stage[attr][question_num] + "\n")
         current_stage[attr][data_num] = check_answer(answer, current_stage[attr][re_num])
 
-def run():
 
+def run():
     for stage in data.keys():
         ask_question(stage)
         print(data[stage]["Name"])
 
+
 run()
-
-
