219268b0f4c0525a22322858d4b4e6e857673e7d
DESKTOP-O10O8FO\kraus <j.kraus@tum.de>
2022-10-31 14:30:10 +0100

    added debug and utils

--- CV-Bot/main.py
+++ CV-Bot/main.py
@@ -1,61 +1,9 @@
 # This is a sample Python script.
 import re
-from termcolor import colored
-
+from utils import *
 
 def check_for_alphabet(string):
     return True
-
-
-# filter for valid inputs by user
-name_re = "([A-Z]|[a-z])[a-z]+ ([A-Z]|[a-z])[a-z]+( ([A-Z]|[a-z])[a-z]+)*"
-date_re = "([0-3]?[0-9].[0-1]?[0-9].[1-2][0-9][0-9][0-9])|([0-3]?[0-9]/[0-1]?[0-9]/[1-2][0-9][0-9][0-9])"
-mail_re = ".+@.+\..+"
-address_re = None
-educ_re = None
-exper_re = None
-social_re = None
-skills_re = None
-interests_re = None
-
-data = {"Personal Data":
-    {
-        "Name": ["What is your full name?", name_re, None],
-        "Birthdate": ["What is your date of birth?", date_re, None],
-        "E-Mail": ["Please tell me your email.", mail_re, None],
-        "Address": ["Please state your address.", address_re, None]
-    },
-    "Education":
-        {
-            "Education_history": ["Please state your educational steps in the form of a "
-                                  "continuous text. For each educational step please enter a time frame, the educational "
-                                  "step and the name of the teaching institution", educ_re, None]
-        },
-    "Experience":
-        {
-            "Experience_history": ["Please state your work experience steps in bullet points or in the form of a "
-                                   "continuous text.", exper_re, None]
-        },
-    "Social Engagement":
-        {
-            "Social_history": ["Please state your social engagements steps in bullet points or in the form of a "
-                               "continuous text.", social_re, None]
-        },
-    "Skills":
-        {
-            "Skills": ["Please state your work experience steps in bullet points or in the form of a "
-                       "continuous text.", skills_re, None]
-        },
-    "Interests":
-        {
-            "Interests": ["If you would like to state any personal interests in your CV please enter them here: ",
-                          interests_re, None]
-        },
-}
-question_num = 0
-re_num = 1
-data_num = 2
-
 
 def check_answer(answer, regex):
     print(answer)
@@ -68,13 +16,13 @@ def check_answer(answer, regex):
 def ask_question(stage):
     current_stage = data[stage]
     for attr in current_stage.keys():
-        answer = input(current_stage[attr][question_num] + "\n")
+        answer = input(current_stage[attr][question_num] + "\n") if not debug else debug_data[stage][attr]
         current_stage[attr][data_num] = check_answer(answer, current_stage[attr][re_num])
 
 
 def print_results():
     indent = "   "
-    cv = ""
+    cv = "C I R R I C U L U M   V I T A E\n"
     for stage in data.keys():
         contains_data = False
         stage_text = stage + ":\n"
@@ -84,7 +32,7 @@ def print_results():
                 stage_text += indent + sub_headline + ":\n"
                 stage_text += indent + indent + data[stage][sub_headline][data_num] + "\n"
         if contains_data:
-            cv += stage_text
+            cv += stage_text + "\n"
     print(cv)
 
 
@@ -92,6 +40,5 @@ def run():
     for stage in data.keys():
         ask_question(stage)
     print_results()
-
 
 run()
---
+++ CV-Bot/utils.py
@@ -0,0 +1,77 @@
+# filter for valid inputs by user
+name_re = "([A-Z]|[a-z])[a-z]+ ([A-Z]|[a-z])[a-z]+( ([A-Z]|[a-z])[a-z]+)*"
+date_re = "([0-3]?[0-9].[0-1]?[0-9].[1-2][0-9][0-9][0-9])|([0-3]?[0-9]/[0-1]?[0-9]/[1-2][0-9][0-9][0-9])"
+mail_re = ".+@.+\..+"
+address_re = None
+educ_re = None
+exper_re = None
+social_re = None
+skills_re = None
+interests_re = None
+
+data = {"Personal Data":
+    {
+        "Name": ["What is your full name?", name_re, None],
+        "Birthdate": ["What is your date of birth?", date_re, None],
+        "E-Mail": ["Please tell me your email.", mail_re, None],
+        "Address": ["Please state your address.", address_re, None]
+    },
+    "Education":
+        {
+            "Education_history": ["Please state your educational steps in the form of a "
+                                  "continuous text. For each educational step please enter a time frame, the educational "
+                                  "step and the name of the teaching institution", educ_re, None]
+        },
+    "Experience":
+        {
+            "Experience_history": ["Please state your work experience steps in bullet points or in the form of a "
+                                   "continuous text.", exper_re, None]
+        },
+    "Social Engagement":
+        {
+            "Social_history": ["Please state your social engagements steps in bullet points or in the form of a "
+                               "continuous text.", social_re, None]
+        },
+    "Skills":
+        {
+            "Skills": ["Please state your work experience steps in bullet points or in the form of a "
+                       "continuous text.", skills_re, None]
+        },
+    "Interests":
+        {
+            "Interests": ["If you would like to state any personal interests in your CV please enter them here: ",
+                          interests_re, None]
+        },
+}
+debug_data = {"Personal Data":
+    {
+        "Name": "Max Mustermann",
+        "Birthdate": "01.01.2001",
+        "E-Mail": "abc@gmail.com",
+        "Address": "Bauerstr. 4"
+    },
+    "Education":
+        {
+            "Education_history": "asdfasdfasdfasdfasdf"
+        },
+    "Experience":
+        {
+            "Experience_history": "asdfasdfasdfasdfasdf"
+        },
+    "Social Engagement":
+        {
+            "Social_history": "asdfasdfasdfasdf"
+        },
+    "Skills":
+        {
+            "Skills": "asdfjashdfkajsdhf"
+        },
+    "Interests":
+        {
+            "Interests": "asdhfakjsdhfasdf"
+        },
+}
+question_num = 0
+re_num = 1
+data_num = 2
+debug = True
